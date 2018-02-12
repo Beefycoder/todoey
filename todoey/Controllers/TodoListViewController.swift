5ed07cd1f2787aed8476d327a689b47f8dfa8f13
Pat Butler <patbutler@node-1w7jr9qlld6m345tvsfkdoxy9.ipv6.telus.net>
2018-02-12 14:59:54 -0800

    Added Codable protocol to model & supporting functions for loading and saving items to a plist to be persisted locally

--- todoey/Controllers/TodoListViewController.swift
+++ todoey/Controllers/TodoListViewController.swift
@@ -10,18 +10,17 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = [Item]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
-    let defaults = UserDefaults.standard
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        print(dataFilePath)
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
+        loadItems()
+        
+
         
     }
 
@@ -34,7 +33,10 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
         return cell
         
     }
@@ -44,17 +46,15 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+  
+        saveItems()
+        
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
     
     //MARK Add New Items
-
     
     @IBAction func AddButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -65,10 +65,12 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once the user clicks the Add Item button on our UIAlert
             
-            self.itemArray.append(textField.text!)
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
-            self.tableView.reloadData()
+            let newItem = Item()
+            newItem.title = textField.text!
             
+            self.itemArray.append(newItem)
+            self.saveItems()
+        
         }
         
         alert.addTextField { (alertTextField) in
@@ -82,10 +84,31 @@ class TodoListViewController: UITableViewController {
         
     }
     
+    //MARK - Model Manipulation Methods
     
+    func saveItems() {
+        
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array!, \(error)")
+        }
+        self.tableView.reloadData()
+    }
     
-    
-    
-    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item, \(error)")
+            }
+            
+        }
+    }
+   
 }
 
---
+++ todoey/Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  todoey
+//
+//  Created by Pat Butler on 2018-02-10.
+//  Copyright © 2018 RPG Ventures. All rights reserved.
+//
+
+import UIKit
+
+class Item: Codable {
+    var title = ""
+    var done = false
+  
+}
